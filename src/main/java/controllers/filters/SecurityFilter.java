240bfad07c21abd35ffe207b648f71993f7d60cc
Randy van Poppel <randy.vanpoppel@student.fontys.nl>
2019-05-31 12:40:43 +0200

    Excluded pinging to REST-endpoint from authentication and fixed issue where authentication header would always be empty

--- src/main/java/controllers/filters/SecurityFilter.java
+++ src/main/java/controllers/filters/SecurityFilter.java
@@ -28,23 +28,25 @@ public class SecurityFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        Response unauthorizedResponse = Response.status(Response.Status.UNAUTHORIZED).build();
-        if (requestContext.getHeaders().get("Authorization") != null) {
-            String tokenString = requestContext.getHeaders().get("Authorization").get(0);
-            tokenString = tokenString.split(" ")[1];
-            HttpResponse<JsonNode> body = null;
-            try {
-                body = Unirest.get("http://localhost:8080/AuthenticationApp-1.0/rest/jsonwebtoken/validate").queryString("tokenString", tokenString).asJson();
-            } catch (UnirestException e) {
-                e.printStackTrace();
-            }
-            if (body.getBody() != null) {
-                String resultobject = body.getBody().toString();
-                JsonParser parser = new JsonParser();
-                JsonElement jsonElement = parser.parse(resultobject);
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
-                String username = jsonObject.get("username").getAsString();
-                // TODO: Fix userDAO injection so Users can be found/created in the DB
+        if (!requestContext.getUriInfo().getPath().contains("ping"))
+        {
+            Response unauthorizedResponse = Response.status(Response.Status.UNAUTHORIZED).build();
+            if (requestContext.getHeaders().get("Authorization") != null) {
+                String tokenString = requestContext.getHeaders().get("Authorization").get(0);
+                tokenString = tokenString.split(" ")[1];
+                HttpResponse<JsonNode> body = null;
+                try {
+                    body = Unirest.get("http://localhost:8080/AuthenticationApp-1.0/rest/jsonwebtoken/validate").queryString("tokenString", tokenString).asJson();
+                } catch (UnirestException e) {
+                    e.printStackTrace();
+                }
+                if (body.getBody() != null) {
+                    String resultobject = body.getBody().toString();
+                    JsonParser parser = new JsonParser();
+                    JsonElement jsonElement = parser.parse(resultobject);
+                    JsonObject jsonObject = jsonElement.getAsJsonObject();
+                    String username = jsonObject.get("username").getAsString();
+                    // TODO: Fix userDAO injection so Users can be found/created in the DB
 //                User user = userDAO.getByUsername(username);
 //                if (user == null)
 //                {
@@ -52,11 +54,10 @@ public class SecurityFilter implements ContainerRequestFilter {
 //                    userDAO.addUser(user);
 //                }
 //                requestContext.getHeaders().add("userId", String.valueOf(user.getId()));
-            } else {
-                requestContext.abortWith(unauthorizedResponse);
+                } else {
+                    requestContext.abortWith(unauthorizedResponse);
+                }
             }
-        } else {
-            requestContext.abortWith(unauthorizedResponse);
         }
     }
 }
