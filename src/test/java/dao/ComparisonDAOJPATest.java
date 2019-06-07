90ba2fa29f5609111636c570cf8f34b785beabf9
Randy van Poppel <randy.vanpoppel@student.fontys.nl>
2019-06-07 13:34:31 +0200

    Created ComparisonDAOJPATests

--- src/test/java/dao/ComparisonDAOJPATest.java
+++ src/test/java/dao/ComparisonDAOJPATest.java
@@ -3,9 +3,11 @@ package dao;
 import dao.jpa.ChoiceDAOJPA;
 import dao.jpa.ComparisonDAOJPA;
 import dao.jpa.UserDAOJPA;
+import dao.jpa.VoteDAOJPA;
 import models.Choice;
 import models.Comparison;
 import models.User;
+import models.Vote;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -34,11 +36,14 @@ public class ComparisonDAOJPATest {
     @InjectMocks
     private ComparisonDAOJPA comparisonDAOJPA;
 
+    @InjectMocks
+    private VoteDAOJPA voteDAOJPA;
+
     private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("localhost_test");
     private EntityManager entityManager;
     private EntityTransaction transaction;
 
-    private User user;
+    private User user1;
     private Choice choice1;
     private Choice choice2;
     private List<Choice> choices;
@@ -59,14 +64,17 @@ public class ComparisonDAOJPATest {
         comparisonDAOJPA = new ComparisonDAOJPA();
         comparisonDAOJPA.setEntityManager(entityManager);
 
-        user = new User(1, "TestUser");
+        voteDAOJPA = new VoteDAOJPA();
+        voteDAOJPA.setEntityManager(entityManager);
+
+        user1 = new User(1, "TestUser1");
         choice1 = new Choice("Choice1");
         choice2 = new Choice("Choice2");
 
         choices = new ArrayList<>();
 
         transaction.begin();
-        user = userDAOJPA.addUser(user);
+        user1 = userDAOJPA.addUser(user1);
         choices.add(choiceDAOJPA.addChoice(choice1));
         choices.add(choiceDAOJPA.addChoice(choice2));
         transaction.commit();
@@ -82,15 +90,59 @@ public class ComparisonDAOJPATest {
     @Test
     public void addComparison()
     {
-        Comparison comparison = new Comparison(user, choices);
+        Comparison comparison = new Comparison(user1, choices);
 
         transaction.begin();
-        Comparison testComparison = comparisonDAOJPA.addComparison(comparison);
+        Comparison testComparison1 = comparisonDAOJPA.addComparison(comparison);
         transaction.commit();
 
-        Assert.assertEquals(testComparison.getUser(), user);
-        List<Choice> testChoices = testComparison.getChoices();
+        Assert.assertEquals(testComparison1.getUser(), user1);
+        List<Choice> testChoices = testComparison1.getChoices();
         Assert.assertEquals(testChoices.get(0), choice1);
         Assert.assertEquals(testChoices.get(1), choice2);
+    }
+
+    @Test
+    public void updateComparison()
+    {
+        Comparison comparison = new Comparison(user1, choices);
+
+        transaction.begin();
+        Comparison testComparison1 = comparisonDAOJPA.addComparison(comparison);
+        transaction.commit();
+
+        Assert.assertTrue(testComparison1.getVotes().isEmpty());
+
+        Vote vote = new Vote(user1, choice1);
+
+        transaction.begin();
+        Vote testVote = voteDAOJPA.addVote(vote);
+        transaction.commit();
+
+        testComparison1.addOrUpdateVote(testVote);
+
+        transaction.begin();
+        Comparison testComparison2 = comparisonDAOJPA.updateComparison(testComparison1);
+        transaction.commit();
+
+        Assert.assertEquals(testComparison2.getVotes().size(), 1);
+    }
+
+    @Test
+    public void getComparisonById()
+    {
+        Comparison comparison = new Comparison(user1, choices);
+
+        transaction.begin();
+        Comparison testComparison1 = comparisonDAOJPA.addComparison(comparison);
+        transaction.commit();
+
+        long id = testComparison1.getId();
+
+        transaction.begin();
+        Comparison testComparison2 = comparisonDAOJPA.getById(id);
+        transaction.commit();
+
+        Assert.assertEquals(testComparison1, testComparison2);
     }
 }
