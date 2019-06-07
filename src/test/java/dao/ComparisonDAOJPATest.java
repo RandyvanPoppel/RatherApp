b0005bdc154e81448c42bdcff365633fb7a9cd5f
Randy van Poppel <randy.vanpoppel@student.fontys.nl>
2019-06-07 13:05:50 +0200

    Created first functioning ComparisonDAOJPATest for adding a Comparison

--- src/test/java/dao/ComparisonDAOJPATest.java
+++ src/test/java/dao/ComparisonDAOJPATest.java
@@ -1,8 +1,15 @@
 package dao;
 
+import dao.jpa.ChoiceDAOJPA;
 import dao.jpa.ComparisonDAOJPA;
+import dao.jpa.UserDAOJPA;
+import models.Choice;
+import models.Comparison;
+import models.User;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import util.DatabaseCleaner;
 
@@ -11,9 +18,18 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ComparisonDAOJPATest {
+
+    @InjectMocks
+    private UserDAOJPA userDAOJPA;
+
+    @InjectMocks
+    private ChoiceDAOJPA choiceDAOJPA;
 
     @InjectMocks
     private ComparisonDAOJPA comparisonDAOJPA;
@@ -22,6 +38,11 @@ public class ComparisonDAOJPATest {
     private EntityManager entityManager;
     private EntityTransaction transaction;
 
+    private User user;
+    private Choice choice1;
+    private Choice choice2;
+    private List<Choice> choices;
+
     @Before
     public void setUp() {
         initMocks(this);
@@ -29,8 +50,26 @@ public class ComparisonDAOJPATest {
         entityManager = entityManagerFactory.createEntityManager();
         transaction = entityManager.getTransaction();
 
+        userDAOJPA = new UserDAOJPA();
+        userDAOJPA.setEntityManager(entityManager);
+
+        choiceDAOJPA = new ChoiceDAOJPA();
+        choiceDAOJPA.setEntityManager(entityManager);
+
         comparisonDAOJPA = new ComparisonDAOJPA();
         comparisonDAOJPA.setEntityManager(entityManager);
+
+        user = new User(1, "TestUser");
+        choice1 = new Choice("Choice1");
+        choice2 = new Choice("Choice2");
+
+        choices = new ArrayList<>();
+
+        transaction.begin();
+        user = userDAOJPA.addUser(user);
+        choices.add(choiceDAOJPA.addChoice(choice1));
+        choices.add(choiceDAOJPA.addChoice(choice2));
+        transaction.commit();
     }
 
     @After
@@ -38,5 +77,20 @@ public class ComparisonDAOJPATest {
         new DatabaseCleaner(entityManager).clean();
         this.entityManager.clear();
         this.entityManager.close();
+    }
+
+    @Test
+    public void addComparison()
+    {
+        Comparison comparison = new Comparison(user, choices);
+
+        transaction.begin();
+        Comparison testComparison = comparisonDAOJPA.addComparison(comparison);
+        transaction.commit();
+
+        Assert.assertEquals(testComparison.getUser(), user);
+        List<Choice> testChoices = testComparison.getChoices();
+        Assert.assertEquals(testChoices.get(0), choice1);
+        Assert.assertEquals(testChoices.get(1), choice2);
     }
 }
